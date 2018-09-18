572dae7296c67b87a6100cfdd1813943572cae56
Norlock <jwillems25@hotmail.com>
2018-09-18 03:44:50 +0200

    added gitignore

--- webapp/seminar-week3-client/webpack.config.js
+++ webapp/seminar-week3-client/webpack.config.js
@@ -1,36 +1,48 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src', 'app.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  module: {
-      rules: [
-          {
-              test: /\.jsx/,
-              use: {
-                  loader: 'babel-loader',
-                  options: { presets: ['react', 'es2015'] }
-              }
-          },
-          {
-              test: /\.scss/,
-              use: ['style-loader', 'css-loader', 'sass-loader']
-          },
-          {
-              test: /\.(png|jpg|gif)$/,
-              use: [
-                  {
-                      loader: 'file-loader',
-                      options: {}
-                  }
-              ]
-          }
-      ]
-  }
+var config = {
+    entry: path.resolve(__dirname, 'src', 'app.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['react', 'es2015'] }
+                }
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = (env, argv) => {
+    if(argv.mode === 'development') {
+        config.optimization = {
+            minimizer: [new UglifyJsPlugin()]
+        };
+    }
+    console.log(config.optimization.minimizer);
+
+    return config;
 };
